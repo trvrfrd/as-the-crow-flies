31f447efa55434b8d2b390ac1f301d70a18d7b52
Trevor Ford <info@trvrfrd.net>
2018-03-25 22:28:13 -0400

    rough in basic text search

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
-var airports;
+var from = document.querySelector("#from");
+var to = document.querySelector("#to");
 
-function loadJSON(path, handler) {
-  console.log(path);
-  var req = new XMLHttpRequest();
-
-  req.addEventListener('load', function resHandler() {
-    alert(this);
+fetch('../data/airports.json')
+  .then(response => response.json())
+  .then(airports => {
+    initTypeAhead(from, airports);
+    initTypeAhead(to, airports);
   });
 
-  req.open('GET', path);
-  req.send();
+function initTypeAhead(elem, arr) {
+  elem.addEventListener("input", function handleInput() {
+    var matches = filterItems(arr, this.value)
+    console.log(matches);
+  });
 }
 
-loadJSON('../data/airports.json', function(json) {
-});
+function filterItems(items, query) {
+  var regexp = new RegExp(query, 'i');
+  return items.filter(item =>
+    Object.values(item).some(field => regexp.test(field))
+  );
+}
