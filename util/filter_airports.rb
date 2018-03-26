2df4bb5913baef4aac061085f2daf476ebb275bc
Trevor Ford <info@trvrfrd.net>
2018-03-25 21:31:56 -0400

    rejigger data-scraping script aka least important part of project

--- util/filter_airports.rb
+++ util/filter_airports.rb
@@ -1,23 +1,35 @@
 #!/usr/bin/env ruby
 #
-# filters a JSON database of the world's airports down to only those that are
-# within the US. Database (not included in this repo) obtained from:
+# Downloads a JSON database of the world's airports and filters it down to only
+# those that are within the US.
+#
+# Database is just some random one I found in this repo:
 # https://github.com/mwgg/Airports
 #
-# Database should be placed in this directory (util/)
-# Script should be run from project root
+# This script is executable and should be run from project root, like so:
+# $ ./util/filter_airports.rb
+#
+# If for some reason it's NOT executable:
+# $ chmod +x ./util/filter_airports.rb
 
 require 'json'
+require 'open-uri'
 
-File.open('./util/airports.json', 'r') do |infile|
-  # airports is a Hash of IATA code => data
-  airports = JSON.load(infile)
-  airports = airports.map { |_, v| v }
-  airports.select! { |a| a["country"] == "US" && !a["iata"].empty? }
-  # keep only necessary fields to reduce file size
-  fields = %w(iata name city lat lon)
-  airports.map! { |a| a.keep_if { |k, _| fields.include?(k) } }
-  File.open('./data/airports.json', 'w') do |outfile|
-    outfile.write(JSON.dump(airports))
+db_url = 'https://raw.githubusercontent.com/mwgg/Airports/master/airports.json'
+out_path = './data/airports.json'
+required_fields = %w(iata name city lat lon)
+
+unless File.exist?(out_path)
+  open(db_url, 'r') do |infile|
+    # airports is a big hash of { IATA code => airport data hash, ...}
+    airports = JSON.load(infile)
+    # make an array of data hashes
+    airports = airports.map { |_, v| v }
+      .select! { |a| a["country"] == "US" && !a["iata"].empty? }
+      .map! { |a| a.keep_if { |k, _| required_fields.include?(k) } }
+    File.open(out_path, 'w') do |outfile|
+      outfile.write(JSON.dump(airports))
+    end
   end
 end
+
