21ba3ca095c4daac23e465d3ca5091b774deea99
Trevor Ford <info@trvrfrd.net>
2018-03-25 23:59:48 -0400

    incremental progress on search UI

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
-var from = document.querySelector("#from");
-var to = document.querySelector("#to");
+var from = document.querySelector('#from');
+var fromResult = document.querySelector('.from__result-list');
+
+var to = document.querySelector('#to');
+var toResult = document.querySelector('.to__result-list');
 
 fetch('../data/airports.json')
   .then(response => response.json())
   .then(airports => {
-    initTypeAhead(from, airports);
-    initTypeAhead(to, airports);
+    console.log(airports[0]);
+    initTypeAhead({ input: from, output: fromResult, data: airports });
+    initTypeAhead({ input: to, output: toResult, data: airports });
   });
 
-function initTypeAhead(elem, arr) {
-  elem.addEventListener("input", function handleInput() {
-    var matches = filterItems(arr, this.value)
-    console.log(matches);
+function initTypeAhead({ input, output, data }) {
+  input.addEventListener('input', function handleInput() {
+    var matches = filterItems(data, this.value).slice(0, 10);
+    var html = matches.map(a => displayAirport(a, this.value)).join('');
+    output.innerHTML = html;
+  });
+
+  output.addEventListener('click', function handleResultClick(e) {
+    if (e.target.nodeName === 'LI') {
+      input.value = e.target.innerText;
+      output.innerHTML = '';
+      // store actual data somewhere: data-lat, data-long ?
+      // calculate distance once both inputs have a result
+    }
   });
 }
 
@@ -21,3 +35,11 @@ function filterItems(items, query) {
     Object.values(item).some(field => regexp.test(field))
   );
 }
+
+function displayAirport(data, highlightText) {
+  var text = `${data.iata} - ${data.name}`;
+  var regexp = new RegExp(highlightText, 'i');
+  text = text.replace(regexp, match => `<span class="highlight">${match}</span>`);
+  return `<li>${text}</li>`;
+}
+
